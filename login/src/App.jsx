689015f77f0cdce56da563a801b5b7d0214de864
Felipe Rivero Kiefer <friverokiefer@gmail.com>
2023-11-12 20:45:34 -0300

    final commit - se prueba y funciona

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -7,41 +7,42 @@ import { BaseColaboradores } from './data/BaseColaboradores';
 import './App.css';
 
 function App() {
-  const [colaboradores, setColaboradores] = useState(BaseColaboradores);
-  const [busqueda, setBusqueda] = useState('');
-  const [alerta, setAlerta] = useState({ mensaje: '', tipo: '' });
+    const [colaboradores, setColaboradores] = useState(BaseColaboradores);
+    const [busqueda, setBusqueda] = useState('');
+    const [alerta, setAlerta] = useState({ mensaje: '', tipo: '' });
 
-  const agregarColaborador = nuevoColaborador => {
-    if (Object.values(nuevoColaborador).some(valor => valor.trim() === '')) {
-      setAlerta({ mensaje: 'Todos los campos deben estar llenos.', tipo: 'danger' });
-      return;
-    }
-    nuevoColaborador.id = Date.now().toString();
-    setColaboradores([...colaboradores, nuevoColaborador]);
-    setAlerta({ mensaje: 'Colaborador agregado con éxito.', tipo: 'success' });
-  };
+    const agregarColaborador = nuevoColaborador => {
+        const camposVacios = Object.values(nuevoColaborador).some(valor => valor.trim() === '');
+        if (camposVacios) {
+            setAlerta({ mensaje: 'Todos los campos deben estar llenos.', tipo: 'danger' });
+        } else {
+            nuevoColaborador.id = Date.now().toString();
+            setColaboradores([...colaboradores, nuevoColaborador]);
+            setAlerta({ mensaje: 'Colaborador agregado con éxito.', tipo: 'success' });
+        }
+    };
 
-  const eliminarColaborador = id => {
-    setColaboradores(colaboradores.filter(col => col.id !== id));
-    setAlerta({ mensaje: 'Colaborador eliminado con éxito.', tipo: 'success' });
-  };
+    const eliminarColaborador = id => {
+        setColaboradores(colaboradores.filter(col => col.id !== id));
+        setAlerta({ mensaje: 'Colaborador eliminado con éxito.', tipo: 'success' });
+    };
 
-  const filtrarColaboradores = textoBusqueda => {
-    setBusqueda(textoBusqueda.toLowerCase());
-  };
+    const filtrarColaboradores = textoBusqueda => {
+        setBusqueda(textoBusqueda.toLowerCase());
+    };
 
-  const colaboradoresFiltrados = colaboradores.filter(col =>
-    Object.values(col).some(valor => valor.toString().toLowerCase().includes(busqueda))
-  );
+    const colaboradoresFiltrados = colaboradores.filter(col =>
+        Object.values(col).some(valor => valor.toString().toLowerCase().includes(busqueda))
+    );
 
-  return (
-    <div className="app-container">
-      {alerta.mensaje && <Alert mensaje={alerta.mensaje} tipo={alerta.tipo} />}
-      <Buscador buscarColaborador={filtrarColaboradores} />
-      <Formulario agregarColaborador={agregarColaborador} />
-      <Listado colaboradores={colaboradoresFiltrados} eliminarColaborador={eliminarColaborador} />
-    </div>
-  );
+    return (
+        <div className="app-container">
+            {alerta.mensaje && <Alert mensaje={alerta.mensaje} tipo={alerta.tipo} />}
+            <Buscador buscarColaborador={filtrarColaboradores} />
+            <Formulario agregarColaborador={agregarColaborador} />
+            <Listado colaboradores={colaboradoresFiltrados} eliminarColaborador={eliminarColaborador} />
+        </div>
+    );
 }
 
 export default App;
--- login/src/components/Formulario/Formulario.jsx
+++ login/src/components/Formulario/Formulario.jsx
@@ -18,75 +18,75 @@ function Formulario({ agregarColaborador }) {
         e.preventDefault();
         agregarColaborador(colaborador);
         setColaborador({
-        nombre: '',
-        correo: '',
-        edad: '',
-        cargo: '',
-        telefono: ''
+            nombre: '',
+            correo: '',
+            edad: '',
+            cargo: '',
+            telefono: ''
         });
     };
 
     return (
         <div className="formulario-container">
-        <h3>Agregar Colaborador</h3>
-        <form onSubmit={handleSubmit} className="form">
-            <div className="form-field">
-            <label htmlFor="nombre">Nombre del colaborador</label>
-            <input
-                type="text"
-                id="nombre"
-                name="nombre"
-                value={colaborador.nombre}
-                onChange={handleChange}
-                required
-            />
-            </div>
-            <div className="form-field">
-            <label htmlFor="correo">Email del colaborador</label>
-            <input
-                type="email"
-                id="correo"
-                name="correo"
-                value={colaborador.correo}
-                onChange={handleChange}
-                required
-            />
-            </div>
-            <div className="form-field">
-            <label htmlFor="edad">Edad del colaborador</label>
-            <input
-                type="text"
-                id="edad"
-                name="edad"
-                value={colaborador.edad}
-                onChange={handleChange}
-                required
-            />
-            </div>
-            <div className="form-field">
-            <label htmlFor="cargo">Cargo del colaborador</label>
-            <input
-                type="text"
-                id="cargo"
-                name="cargo"
-                value={colaborador.cargo}
-                onChange={handleChange}
-                required
-            />
-            </div>
-            <div className="form-field">
-            <label htmlFor="telefono">Teléfono del colaborador</label>
-            <input
-                type="text"
-                id="telefono"
-                name="telefono"
-                value={colaborador.telefono}
-                onChange={handleChange}
-                required
-            />
-            </div>
-            <button type="submit" className="btn-agregar">Agregar Colaborador</button>
-        </form>
+            <h3>Agregar Colaborador</h3>
+            <form onSubmit={handleSubmit} className="form">
+                <div className="form-field">
+                    <label htmlFor="nombre">Nombre del colaborador</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        name="nombre"
+                        value={colaborador.nombre}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="correo">Email del colaborador</label>
+                    <input
+                        type="email"
+                        id="correo"
+                        name="correo"
+                        value={colaborador.correo}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="edad">Edad del colaborador</label>
+                    <input
+                        type="text"
+                        id="edad"
+                        name="edad"
+                        value={colaborador.edad}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="cargo">Cargo del colaborador</label>
+                    <input
+                        type="text"
+                        id="cargo"
+                        name="cargo"
+                        value={colaborador.cargo}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="telefono">Teléfono del colaborador</label>
+                    <input
+                        type="text"
+                        id="telefono"
+                        name="telefono"
+                        value={colaborador.telefono}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <button type="submit" className="btn-agregar">Agregar Colaborador</button>
+            </form>
         </div>
     );
 }
